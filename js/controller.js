255f96080f41a153d83941ef4b11dacc644c3197
paninapress <npacifico25@gmail.com>
2016-05-26 17:24:38 -0700

    can change size from index page

--- js/controller.js
+++ js/controller.js
@@ -9,29 +9,34 @@ angular.module('gridPartyApp', [])
 	var maxTileSize = 80;
 
 	// set these as we want
-	var gridLengthX = 32;
-	var gridLengthY = 9;
+	// var gridLengthX = 24;
+	$scope.gridSize = {x: null, y: null}
+	// var gridLengthY = 9;
 
-	// create tiles
-	for (i = 1; i <= gridLengthX * gridLengthY; i++){
-		board.append('<div class="tile">'+i+'</div>')
-		
-	}
+	$scope.createTiles = function(){
+		if ($scope.gridSize.x != null && $scope.gridSize.y != null){
+			// create tiles
+			for (i = 1; i <= $scope.gridSize.x * $scope.gridSize.y; i++){
+				board.append('<div class="tile">'+i+'</div>')
+				
+			}
 
-	// set game and tile size, based on window size for now
-	var windowWidth = $(window).width();
-	
-	var tileSize = Math.floor(windowWidth / gridLengthX);
-	//avoid big tiles
-	if (tileSize > maxTileSize) {
-		board.css('width', gridLengthX * maxTileSize);
-		//  -4 is for the border
-		$('.tile').css('width', maxTileSize - 4).css('height', maxTileSize - 4);
-	}
-	else{ 
-		board.css('width', tileSize * gridLengthX);
-		// - 4 is for border
-		$('.tile').css('width', tileSize - 4).css('height', tileSize - 4 );
+			// set game and tile size, based on window size for now
+			var windowWidth = $(window).width();
+			
+			var tileSize = Math.floor(windowWidth / $scope.gridSize.x);
+			//avoid big tiles
+			if (tileSize > maxTileSize) {
+				board.css('width', $scope.gridSize.x * maxTileSize);
+				//  -4 is for the border
+				$('.tile').css('width', maxTileSize - 4).css('height', maxTileSize - 4);
+			}
+			else{ 
+				board.css('width', tileSize * $scope.gridSize.x);
+				// - 4 is for border
+				$('.tile').css('width', tileSize - 4).css('height', tileSize - 4 );
+			};
+		};	
 	};
 
 });
